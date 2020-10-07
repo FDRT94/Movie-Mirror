87674523d45e956178796611a0be3ad9205efc07
Frank de Raadt <31472893+FDRT94@users.noreply.github.com>
2020-10-07 17:41:04 +0200

    Updated Security

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -16,7 +16,7 @@ class SearchDropdown extends Component
 		// only search if 2 or more characters
 		if (strlen($this->search) >= 2)
 		{
-			$searchResults = Http::withToken('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjgzNTViY2RmYWUzNGE2ZTQ3YTM2YmExZmFkMWFjOCIsInN1YiI6IjVmNzYzNTlkODc0MWM0MDAzODY4YTYyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZzY0BrybSKc1uylB_-l6Km8_PLo6B_JB7NCY__4_488')
+			$searchResults = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/search/movie?query='.$this->search)
 			->json()['results'];
 		}
