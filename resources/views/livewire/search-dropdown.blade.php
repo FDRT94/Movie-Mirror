57d7c59670d8e9f2db45ed435d4b5d25df7d347b
FDRT94 <99046164@mydavinci.nl>
2020-10-15 16:26:30 +0200

    Add UI Interactivity with Alpine.js

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,8 +1,9 @@
-<div class="relative mt-3 md:mt-0" x-data:"{ isOpen = true }">
+<div class="relative mt-3 md:mt-0" x-data:"{ isOpen=true }" @click.away="isOpen = false">
     <input wire:model.debounce.500ms="search" type="text"
         class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
-        placeholder="Search"
+        placeholder="Search" 
         @focus="isOpen = true"
+        @keydown="isOpen = true"
         @keydown.escape.window="isOpen = false"
         @keydown.shift.tab="isOpen = false"
         >
@@ -13,25 +14,29 @@
         </svg>
     </div>
 
-	<div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
+    <div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
 
     @if (strlen($search) >= 2)
-    <div class="absolute bg-gray-800 rounded text-sm w-64 mt-4">
+    <div class="z-50 absolute bg-gray-800 rounded text-sm w-64 mt-4" x-show.transition.opacity="isOpen">
         @if ($searchResults->count() > 0)
         <ul>
             @foreach ($searchResults as $result)
             <li class="z-50 border-b border-gray-700">
                 <a href="{{ route('movies.show', $result['id']) }}"
-                    class="block hover:bg-gray-700 flex items-center px-3 py-3 ">
+                    class="block hover:bg-gray-700 flex items-center px-3 py-3 " @if ($loop->last)
+                    @keydown.tab="isOpen = false"
+                    @endif
+
+                    >
                     {{-- if no img available show placeholder img --}}
                     @if ($result['poster_path'])
                     <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster" class="w-8">
                     @else
-					<img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
-					@endif
-					<span class="ml-4">{{ $result['title'] }}</span> 	
-				</a>
-                
+                    <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+                    @endif
+                    <span class="ml-4">{{ $result['title'] }}</span>
+                </a>
+
             </li>
             @endforeach
         </ul>
