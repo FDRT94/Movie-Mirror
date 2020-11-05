b3808f709c936dd2ebd97e6de348a63043260b7b
FDRT94 <99046164@mydavinci.nl>
2020-11-05 21:47:38 +0100

    PurgeCSS and Deployment

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,9 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-	.postCss('resources/css/main.css', 'public/css', [
-		require('tailwindcss'),
-    ]);
+// deleted mix.js because project doesnt need JS to function properly
+mix.postCss('resources/css/main.css', 'public/css', [
+    require('tailwindcss'),
+])
+.purgeCss();
+    
